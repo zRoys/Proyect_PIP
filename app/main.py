896ba8be4adb8676c48634c2d9bb5465cda514ce
Roy <roysteve.nc@gmail.com>
2023-06-01 12:45:21 -0600

    add web-server and app pandas

--- app/main.py
+++ app/main.py
@@ -1,15 +1,27 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 def run():
+  '''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item : item['Continent'] == 'South America',data))
 
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
+  
+  '''
+  
+  df = pd.read_csv('data.csv')
+  df = df[df['Continent']=='Africa']
+  
+  countries = df['Country/Territory']
+  percentages = df['World Population Percentage']
+  
   charts.generate_pie_chart(countries, percentages)
   
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
   print(country)
 
