cd6724345bbf22d6ae8ab5d45463603dba0f3a97
Roy <roysteve.nc@gmail.com>
2023-06-01 12:46:01 -0600

    add pandas and web server

---
+++ web-server/store.py
@@ -0,0 +1,10 @@
+import requests
+
+def get_categories():
+    r = requests.get('https://api.escuelajs.co/api/v1/categories')
+    print(r.status_code)
+    print(r.text)
+    print(type(r.text))
+    categories = r.json()
+    for categories in categories:
+        print(categories['name'])
